de7e0e7e0062f2f319e2089defb488dbe119c8b8
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-05 00:10:41 -0300

    faz uma limpa

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 
-async function bootstrap() {
+async function createApp() {
   const app = await NestFactory.create(AppModule)
+
   await app.listen(3000)
 }
-bootstrap()
+
+createApp()
