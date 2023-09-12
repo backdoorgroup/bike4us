b15066d8842dfd61857843bf0365bc237c726ec3
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-12 19:09:58 -0300

    nest config

--- backend/src/bike4us/project/settings.ts
+++ backend/src/bike4us/project/settings.ts
@@ -1,12 +1,13 @@
 export const settings = {
+  // Nest
+  NEST_PORT: process.env.NEST_PORT || 8000,
+
   // Database
   DB_NAME: process.env.DB_NAME || "postgres",
   DB_USER: process.env.DB_USER || "postgres",
   DB_PASSWORD: process.env.DB_PASSWORD || "postgres",
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_PORT: process.env.DB_PORT || 5432,
-
-  PORT: process.env.NEST_PORT || 8000,
 
   // Firebase
   FB_PROJECT_ID: process.env.FB_PROJECT_ID,
