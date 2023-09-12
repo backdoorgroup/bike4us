2c5bd2221422dbec42be21de0efde5ca88ba629e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-11 23:18:01 -0300

    app bonitinho

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { App } from './app'
+
 const rootEl = document.getElementById('root')!
 const root =  ReactDOM.createRoot(rootEl)
 
