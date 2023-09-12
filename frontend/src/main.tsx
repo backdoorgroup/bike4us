4351cfd1209b8edad68122766cb7cc6578e89139
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-11 23:10:19 -0300

    faz uma limpa

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root')!
+const root =  ReactDOM.createRoot(rootEl)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
