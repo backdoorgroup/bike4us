75a8fa762604ef95dc393df4fc93ff411bb85a80
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-12 20:03:42 -0300

    auth persiste

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,3 +1,42 @@
+import { useState } from "react"
+
+import { initializeApp } from "firebase/app"
+import { getAuth, signInWithEmailAndPassword, setPersistence, browserLocalPersistence } from "firebase/auth"
+
+const app = initializeApp({
+  apiKey: import.meta.env._FB_API_KEY,
+  authDomain: import.meta.env._FB_AUTH_DOMAIN,
+  projectId: import.meta.env._FB_PROJECT_ID,
+  storageBucket: import.meta.env._FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env._FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env._FB_APP_ID
+})
+
+const auth = getAuth(app)
+
+setPersistence(auth, browserLocalPersistence)
+
 export default function App() {
-  return <div>hello world!</div>
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [user, setUser] = useState(null)
+
+  auth.onAuthStateChanged((user) => setUser(user))
+
+  return (
+    <main>
+      <pre>{JSON.stringify(user, null, 4)}</pre>
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault()
+          await signInWithEmailAndPassword(auth, email, password)
+        }}>
+        <input onChange={(e) => setEmail(e.target.value)} />
+        <input onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Sign in</button>
+      </form>
+    </main>
+  )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  }
+  },
+  envPrefix: "_"
 })
