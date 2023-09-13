2711a638124a4e8f8f596265696b0394358f15af
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-13 00:50:19 -0300

    database config

--- backend/src/bike4us/project/database.ts
+++ backend/src/bike4us/project/database.ts
@@ -4,10 +4,19 @@ import { settings } from "bike4us/project/settings"
 
 export const DatabaseModule = TypeOrmModule.forRoot({
   type: "postgres",
+
   host: settings.DB_HOST,
   port: settings.DB_PORT,
   username: settings.DB_USER,
   password: settings.DB_PASSWORD,
   database: settings.DB_NAME,
-  synchronize: settings.NEST_MODE === "dev"
+
+  synchronize: settings.NEST_MODE === "dev",
+
+  entities: ["**/models.ts"],
+
+  migrations: ["src/bike4us/project/migrations/*.ts"],
+  migrationsTableName: "migration",
+
+  ssl: settings.NEST_MODE === "prod"
 })
