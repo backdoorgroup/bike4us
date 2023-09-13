63111ef1b792744a0201a1d16a9aaf95f6bcaa1e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-12 22:37:48 -0300

    null

--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -3,14 +3,13 @@ import { create } from "zustand"
 import type { User } from "firebase/auth"
 
 interface State {
-  user?: User
+  user: User | null
 }
-
 interface Actions {
   setUser: (user: User) => void
 }
 
 export const useUserStore = create<State & Actions>((set) => ({
-  user: undefined,
+  user: null,
   setUser: (user: User) => set(() => ({ user }))
 }))
