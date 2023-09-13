5ba23c557d73236ddc871970725b80e28dea92fb
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-12 21:48:21 -0300

    vai

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite"
+import { URL, fileURLToPath } from "node:url"
 
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: "_",
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 3000
   },
-  envPrefix: "_"
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  publicDir: "./src/public"
 })
