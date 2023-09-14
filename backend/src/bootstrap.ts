52dad26fe6613bd05f4c5aab4eadf7d403370f0e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-14 18:13:50 -0300

    bootstrap funciona

--- backend/src/bootstrap.ts
+++ backend/src/bootstrap.ts
@@ -1,24 +1,14 @@
 import helmet from "helmet"
 import cors from "cors"
+import express from "express"
 
-import { NestFactory } from "@nestjs/core"
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
-
-import { AppModule } from "~/module"
 import { settings } from "~/settings"
 
 export const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule)
-
-  app.setGlobalPrefix("/api/v1")
+  const app = express()
 
   app.use(cors())
   app.use(helmet())
 
-  const config = new DocumentBuilder().build()
-  const document = SwaggerModule.createDocument(app, config)
-
-  SwaggerModule.setup("docs", app, document)
-
-  await app.listen(settings.NEST_PORT)
+  app.listen(settings.EXPRESS_PORT)
 }
--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   // Express
-  EXPRESS_PORT: parseInt(process.env.EXPRESS_PORT) || 8000,
+  EXPRESS_PORT: parseInt(process.env.EXPRESS_PORT!) || 8000,
   EXPRESS_MODE: process.env.EXPRESS_MODE || "dev",
 
   // Database
@@ -8,7 +8,7 @@ export const settings = {
   DB_USER: process.env.DB_USER || "postgres",
   DB_PASSWORD: process.env.DB_PASSWORD || "postgres",
   DB_HOST: process.env.DB_HOST || "localhost",
-  DB_PORT: parseInt(process.env.DB_PORT) || 5432,
+  DB_PORT: parseInt(process.env.DB_PORT!) || 5432,
 
   // Firebase
   FB_PROJECT_ID: process.env.FB_PROJECT_ID,
