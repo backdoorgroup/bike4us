73d5366306f8a38aa42ed36a9f60f3e3b16dda0e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-15 10:32:40 -0300

    feat adiciona searchbar

--- frontend/src/layouts/HomeLayout.tsx
+++ frontend/src/layouts/HomeLayout.tsx
@@ -3,6 +3,9 @@ import { Outlet } from "react-router-dom"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Icon from "@mui/material/Icon"
+import TextField from "@mui/material/TextField"
+import InputAdornment from "@mui/material/InputAdornment"
+// import Button from "@mui/material/Button"
 
 export function LandingLayout() {
   return (
@@ -14,7 +17,19 @@ export function LandingLayout() {
         elevation={0}
         sx={{ borderBottom: 1, borderColor: "lightgray" }}>
         <Toolbar>
-          <Icon>directions_bike</Icon>
+          <Icon sx={{ marginRight: 2 }}>directions_bike</Icon>
+          <TextField
+            fullWidth
+            size="small"
+            placeholder="Encontrar"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Icon>search</Icon>
+                </InputAdornment>
+              )
+            }}
+          />
         </Toolbar>
       </AppBar>
       <main>
