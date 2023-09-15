fe956d6e5c798984a85e9f1551135cfd4f3c269d
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-15 18:36:41 -0300

    card

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -25,27 +25,29 @@ export function HomePage() {
             Anúncios recentes
           </Typography>
         </Container>
-        <Stack direction="row" overflow="scroll" gap="16px" paddingX="16px">
-          {[0, 1, 2, 3, 4].map((_, index) => (
-            <Card key={index} sx={{ minWidth: 256, width: 256, border: 1, borderColor: "grey.300" }} elevation={0}>
-              <CardActionArea>
-                <CardMedia sx={{ height: 128 }} image="https://loremflickr.com/128/128/animals" />
-                <CardContent>
-                  <Typography variant="subtitle2">Vila Pinheiro, São Paulo</Typography>
-                  <Typography variant="caption" component="p" color="text.secondary">
-                    10 horas atrás
-                  </Typography>
-                  <Typography variant="subtitle1" component="span" sx={{ fontWeight: 700 }}>
-                    R$300
-                  </Typography>
-                  <Typography variant="caption" component="span">
-                    /dia
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))}
-        </Stack>
+        <Container>
+          <Stack direction="row" overflow="auto" gap="16px">
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((_, index) => (
+              <Card key={index} sx={{ minWidth: 256, width: 256, border: 1, borderColor: "grey.300" }} elevation={0}>
+                <CardActionArea>
+                  <CardMedia sx={{ height: 128 }} image="https://loremflickr.com/128/128/animals" />
+                  <CardContent>
+                    <Typography variant="subtitle2">Vila Pinheiro, São Paulo</Typography>
+                    <Typography variant="caption" component="p" color="text.secondary">
+                      10 horas atrás
+                    </Typography>
+                    <Typography variant="subtitle1" component="span" sx={{ fontWeight: 700 }}>
+                      R$300
+                    </Typography>
+                    <Typography variant="caption" component="span">
+                      /dia
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
+          </Stack>
+        </Container>
       </Box>
     </>
   )
