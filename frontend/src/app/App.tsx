b8b5295b2f365ca9607a6e169efcb77d1eec644a
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-14 23:57:39 -0300

    base

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,26 @@
+import StyledEngineProvider from "@mui/material/StyledEngineProvider"
+import CssBaseline from "@mui/material/CssBaseline"
+
+import createTheme from "@mui/material/styles/createTheme"
+import ThemeProvider from "@mui/material/styles/ThemeProvider"
+
 import { RouterProvider as Router } from "react-router-dom"
 
 import { router } from "~/router"
 
+const theme = createTheme({
+  palette: {
+    mode: "light"
+  }
+})
+
 export function App() {
   return (
-    <>
-      <Router router={router} />
-    </>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router router={router} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   )
 }
