67f1da6a0e7a4c0eec02bed629c4c60283d6a874
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-15 12:30:56 -0300

    card

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,22 +4,38 @@ import InputAdornment from "@mui/material/InputAdornment"
 
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
+import Card from "@mui/material/Card"
+import Typography from "@mui/material/Typography"
 
 export function HomePage() {
   return (
-    <Box>
-      <Container sx={{ marginY: 4 }}>
-        <TextField
-          fullWidth
-          placeholder="Encontrar"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Icon>search</Icon>
-              </InputAdornment>
-            )
-          }}
-        />
+    <Box sx={{ bgcolor: "primary.main", color: "common.white" }}>
+      <Container sx={{ paddingY: 2 }}>
+        <Typography variant="h6" component="h1">
+          Encontre o anunciado perfeito
+        </Typography>
+        <Typography variant="body2" component="h2" mb={2}>
+          Descubra o padrão ouro em aluguel de bicicletas
+        </Typography>
+
+        <Card elevation={0} sx={{ padding: 2 }}>
+          <Typography variant="h6" component="h3" mb={1}>
+            Encontre um anunciado
+          </Typography>
+
+          <TextField
+            fullWidth
+            size="small"
+            placeholder="O que você procura?"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Icon>search</Icon>
+                </InputAdornment>
+              )
+            }}
+          />
+        </Card>
       </Container>
     </Box>
   )
