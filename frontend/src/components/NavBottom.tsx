c6940271f0c9c8f4a3c8c88d14a527b286ac021e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-15 11:45:13 -0300

    nav bootm

--- frontend/src/components/NavBottom.tsx
+++ frontend/src/components/NavBottom.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
+
 import BottomNavigation from "@mui/material/BottomNavigation"
 import BottomNavigationAction from "@mui/material/BottomNavigationAction"
+import Icon from "@mui/material/Icon"
 
 export function NavBottom() {
+  const [value, setValue] = useState(0)
+
   return (
     <BottomNavigation
       sx={{ borderTop: 1, borderColor: "lightgray", position: "fixed", bottom: 0, left: 0, right: 0 }}
       component="footer"
-      showLabels>
-      <BottomNavigationAction label="Home" />
+      showLabels
+      value={value}
+      onChange={(_, value) => setValue(value)}>
+      <BottomNavigationAction label="Encontrar" icon={<Icon>search</Icon>} />
+      <BottomNavigationAction label="Anunciar" icon={<Icon>add_circle_outline</Icon>} />
+      <BottomNavigationAction label="Minha conta" icon={<Icon>person</Icon>} />
     </BottomNavigation>
   )
 }
