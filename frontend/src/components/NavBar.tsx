08e12096be1ec8b845c9dfcb33b0682da6d04376
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-15 11:49:23 -0300

    vai

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,6 @@
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Icon from "@mui/material/Icon"
-import TextField from "@mui/material/TextField"
-import InputAdornment from "@mui/material/InputAdornment"
 
 export function NavBar() {
   return (
@@ -14,18 +12,6 @@ export function NavBar() {
       sx={{ borderBottom: 1, borderColor: "lightgray" }}>
       <Toolbar>
         <Icon sx={{ marginRight: 2 }}>directions_bike</Icon>
-        <TextField
-          fullWidth
-          size="small"
-          placeholder="Encontrar"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Icon>search</Icon>
-              </InputAdornment>
-            )
-          }}
-        />
       </Toolbar>
     </AppBar>
   )
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,26 @@
+import Icon from "@mui/material/Icon"
+import TextField from "@mui/material/TextField"
+import InputAdornment from "@mui/material/InputAdornment"
+
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+
 export function HomePage() {
-  return <div>I am a home page</div>
+  return (
+    <Box>
+      <Container sx={{ marginY: 4 }}>
+        <TextField
+          fullWidth
+          placeholder="Encontrar"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Icon>search</Icon>
+              </InputAdornment>
+            )
+          }}
+        />
+      </Container>
+    </Box>
+  )
 }
