cfa824b744608594d6a60c41cb5b12881cf5803b
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-15 00:18:23 -0300

    home page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,3 @@
+export function HomePage() {
+  return <div>I am a home page</div>
+}
--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,10 +1,17 @@
 import type { RouteObject } from "react-router-dom"
 
 import { LandingLayout } from "~/layouts"
+import { HomePage } from "~/pages"
 
 export const routes: RouteObject[] = [
   {
     path: "/",
-    element: <LandingLayout />
+    element: <LandingLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }
+    ]
   }
 ]
