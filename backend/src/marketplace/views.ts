cc3222ff3e51082e38d86072e5067839910756c2
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-22 20:29:26 -0300

    paginação

--- backend/src/marketplace/views.ts
+++ backend/src/marketplace/views.ts
@@ -6,6 +6,7 @@ import { getListings } from "@/marketplace/services"
 export const router = Router()
 
 router.get("/listings", async (req, res) => {
+  // TODO: isso aqui precisa de paginação
   const query = await getListings()
   const listings = query.map((listing) => serializeListing(listing))
 
