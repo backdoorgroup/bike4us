4b869ca6d6f9c6469089c2a394459ae5e0d6e41f
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-24 00:51:01 -0300

    n precisa do min

--- backend/src/schemas.ts
+++ backend/src/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const PaginationSchema = z.object({
-  page: z.coerce.number().int().positive().min(1).optional().default(1),
-  perPage: z.coerce.number().int().positive().min(1).max(100).optional().default(15)
+  page: z.coerce.number().int().positive().optional().default(1),
+  perPage: z.coerce.number().int().positive().max(100).optional().default(15)
 })
 export type TPaginationSchema = z.infer<typeof PaginationSchema>
