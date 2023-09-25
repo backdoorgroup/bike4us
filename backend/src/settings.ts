7756c7e4d38b58a3b7168581c790002d7f4ac614
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-25 15:10:48 -0300

    settings

--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -1,16 +1,22 @@
 const env = process.env
 
 export const settings = {
+  // Express
   EXPRESS_PORT: parseInt(env.EXPRESS_PORT!) || 8000,
   EXPRESS_HOST: env.EXPRESS_HOST || "0.0.0.0",
   EXPRESS_MODE: env.EXPRESS_MODE || "dev",
 
+  // Database Connection
   DB_NAME: env.DB_NAME || "postgres",
   DB_USER: env.DB_USER || "postgres",
   DB_PASSWORD: env.DB_PASSWORD || "postgres",
   DB_HOST: env.DB_HOST || "localhost",
   DB_PORT: parseInt(env.DB_PORT!) || 5432,
 
+  // Database Configuration
+  DB_DROP_SCHEMA: env.DB_DROP_SCHEMA === "true" || false,
+
+  // Firebase
   FB_PROJECT_ID: env.FB_PROJECT_ID,
   FB_CLIENT_EMAIL: env.FB_CLIENT_EMAIL,
   FB_PRIVATE_KEY: env.FB_PRIVATE_KEY
