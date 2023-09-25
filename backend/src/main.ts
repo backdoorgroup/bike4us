9f5b03f5ac68eec443f50eeb4ca84a98f38b8ec8
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-09-25 18:37:21 -0300

    profile

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import express from "express"
 import helmet from "helmet"
 import pino from "pino-http"
 
-import { identity, authenticated } from "@/auth/middlewares"
+import { identity, authenticated } from "@/profile/middlewares"
 import { dataSource } from "@/database"
 import { logger } from "@/logger"
 import { router } from "@/router"
--- backend/src/profile/middlewares.ts
+++ backend/src/profile/middlewares.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Request, Response } from "express"
 import type { UserRecord } from "firebase-admin/auth"
 
-import { auth } from "@/auth/services"
+import { auth } from "@/profile/services"
 import { UnauthorizedException } from "@/exceptions"
 import { HttpStatus } from "@lib/http"
 
