ea07a0eaf8723436ecd8fa5a5ad799e888ab7a0e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-10-01 22:36:02 -0300

    return

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -4,15 +4,15 @@ import { authClient, googleProvider } from "@/services/clients"
 
 export const AuthService = {
   signInWithEmailAndPassword: async (email: string, password: string) => {
-    await signInWithEmailAndPassword(authClient, email, password)
+    return await signInWithEmailAndPassword(authClient, email, password)
   },
   signInWithGooglePopup: async () => {
-    await signInWithPopup(authClient, googleProvider)
+    return await signInWithPopup(authClient, googleProvider)
   },
   signOut: async () => {
-    await signOut(authClient)
+    return await signOut(authClient)
   },
   createUserWithEmailAndPassword: async (email: string, password: string) => {
-    await createUserWithEmailAndPassword(authClient, email, password)
+    return await createUserWithEmailAndPassword(authClient, email, password)
   }
 }
