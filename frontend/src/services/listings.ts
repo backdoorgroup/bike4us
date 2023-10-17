afff4a9360c7bd5ea19e68d08234b536ca5e20ad
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-10-17 18:49:23 -0300

    todo

--- frontend/src/services/listings.ts
+++ frontend/src/services/listings.ts
@@ -2,6 +2,7 @@ import { httpClient } from "@/services/clients"
 import { ListingForm } from "@/schemas"
 
 export const ListingsServices = {
+  // TODO: tipar com zod
   createListing: async (listing: ListingForm) => {
     return await httpClient.post("/listings", listing)
   },
