32390ef4430dee4cc98ef4b8a5cf79f452eba66f
rafaelres <rafaelsiqueirinha@gmail.com>
2023-10-17 21:13:40 -0300

    listings done

--- frontend/src/pages/AnnouncePage.tsx
+++ frontend/src/pages/AnnouncePage.tsx
@@ -1,14 +1,5 @@
-import { ChangeEventHandler, useState } from "react"
 import { useForm } from "react-hook-form"
 
-import styled from "@mui/material/styles/styled"
-
-import Alert, { type AlertColor as TSeverity } from "@mui/material/Alert"
-import Box from "@mui/material/Box"
-import ButtonBase from "@mui/material/ButtonBase"
-import Icon from "@mui/material/Icon"
-import ImageListItem from "@mui/material/ImageListItem"
-import Collapse from "@mui/material/Collapse"
 import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
 import TextField from "@mui/material/TextField"
@@ -16,57 +7,19 @@ import FormControl from "@mui/material/FormControl"
 import Stack from "@mui/material/Stack"
 import Button from "@mui/material/Button"
 
-import { TitleValidation, DescriptionValidation, HourPricingValidation, ListingForm } from "@/schemas"
+import { TitleValidation, TypeValidation, QuadroValidation, AroValidation, TimeValidation, ConditionValidation, BrandValidation, ColorValidation, DescriptionValidation, HourPricingValidation, ListingForm } from "@/schemas"
 import { ListingsServices } from "@/services"
-
-const imageSize = 128
-
-const HiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1
-})
 
 export function AnnouncePage() {
   const form = useForm<ListingForm>()
 
-  const [images, setImages] = useState<File[]>([])
-  const [loading, setLoading] = useState(false)
-  const [alert, setAlert] = useState<{ title: string; severity: TSeverity }>()
-
   const handleSubmit = async (listing: ListingForm) => {
-    try {
-      /*
-       *  1. Post pro backend que cria o anúncio
-       *  2. Vai pra página do anúncio criado
-       */
+    /*
+     *  1. Post pro backend que cria o anúncio
+     *  2. Vai pra página do anúncio criado
+     */
 
-      setLoading(true)
-
-      await ListingsServices.createListing(listing)
-
-      setAlert({
-        title: "Seu anúncio foi criado com sucesso!",
-        severity: "success"
-      })
-    } catch (error) {
-      setAlert({
-        title: "Ocorreu um erro ao criar seu anúncio",
-        severity: "error"
-      })
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  const handleImage: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setImages([...images, ...(e.target.files ?? [])])
+    await ListingsServices.createListing(listing)
   }
 
   return (
@@ -77,68 +30,61 @@ export function AnnouncePage() {
 
       <FormControl component="form" fullWidth noValidate autoComplete="off" onSubmit={form.handleSubmit(handleSubmit)}>
         <Stack gap={2} marginBottom={4}>
-          <Collapse in={!!alert?.title} unmountOnExit>
-            <Alert severity={alert?.severity}>{alert?.title}</Alert>
-          </Collapse>
-
-          <Stack sx={{ overflowX: "auto", flexDirection: "row" }} gap={2}>
-            <ButtonBase
-              component="label"
-              sx={{
-                minHeight: imageSize,
-                maxHeight: imageSize,
-                minWidth: imageSize,
-                maxWidth: imageSize,
-                border: 1,
-                borderRadius: 1,
-                borderColor: "primary.main",
-                borderStyle: "dashed",
-                color: "primary.main"
-              }}>
-              <Box sx={{ fontSize: 32, textAlign: "center" }}>
-                <Icon fontSize="inherit">add_a_photo</Icon>
-
-                <Typography variant="subtitle2" component="p">
-                  Adicionar fotos
-                </Typography>
-
-                <Typography variant="caption" component="p" fontSize="10px">
-                  Somente JPG, JPEG e PNG
-                </Typography>
-              </Box>
-
-              <HiddenInput type="file" accept="image/png, image/jpeg, image/jpg" onChange={handleImage} />
-            </ButtonBase>
-
-            {Object.values(images).map((image, index) => (
-              <ImageListItem
-                key={index}
-                sx={{
-                  borderRadius: 1,
-                  overflow: "hidden",
-                  minWidth: imageSize,
-                  maxWidth: imageSize
-                }}>
-                <img
-                  src={URL.createObjectURL(image)}
-                  loading="lazy"
-                  style={{
-                    minWidth: imageSize,
-                    maxWidth: imageSize,
-                    minHeight: imageSize,
-                    maxHeight: imageSize
-                  }}
-                />
-              </ImageListItem>
-            ))}
-          </Stack>
-
           <TextField
             label="Título"
             placeholder="Digite o título de seu anúncio"
             error={!!form.formState.errors.title}
             helperText={form.formState.errors.title?.message}
             {...form.register("title", TitleValidation)}
+          />
+          <TextField
+            label="Tipo de bicicleta"
+            placeholder="Ex: Mountain bike/MTB, BMX, Speed, Infantil, Passeio, Urbano, Elétrica, etc…"
+            error={!!form.formState.errors.type}
+            helperText={form.formState.errors.type?.message}
+            {...form.register("type", TypeValidation)}
+          />
+          <TextField
+            label="Quadro"
+            placeholder="Ex: S (15” - 16”), M (16” - 18”), L (19”), XL (21”)"
+            error={!!form.formState.errors.quadro}
+            helperText={form.formState.errors.quadro?.message}
+            {...form.register("quadro", QuadroValidation)}
+          />
+          <TextField
+            label="Aro"
+            placeholder="Ex: 12”, 16”, 20”, 24”, 26”"
+            error={!!form.formState.errors.aro}
+            helperText={form.formState.errors.aro?.message}
+            {...form.register("aro", AroValidation)}
+          />
+          <TextField
+            label="Tempo de uso"
+            placeholder="Digite o tempo de uso de sua bicicleta"
+            error={!!form.formState.errors.time}
+            helperText={form.formState.errors.time?.message}
+            {...form.register("time", TimeValidation)}
+          />
+          <TextField
+            label="Condição"
+            placeholder="Digite a condição de sua bicicleta"
+            error={!!form.formState.errors.condition}
+            helperText={form.formState.errors.condition?.message}
+            {...form.register("condition", ConditionValidation)}
+          />
+          <TextField
+            label="Marca"
+            placeholder="Digite a marca de sua bicicleta"
+            error={!!form.formState.errors.brand}
+            helperText={form.formState.errors.brand?.message}
+            {...form.register("brand", BrandValidation)}
+          />
+          <TextField
+            label="Cor"
+            placeholder="Digite a cor de sua bicicleta"
+            error={!!form.formState.errors.color}
+            helperText={form.formState.errors.color?.message}
+            {...form.register("color", ColorValidation)}
           />
           <TextField
             label="Descrição"
@@ -160,7 +106,7 @@ export function AnnouncePage() {
           />
         </Stack>
 
-        <Button type="submit" variant="contained" disabled={loading} disableElevation>
+        <Button type="submit" variant="contained" disableElevation>
           Anunciar
         </Button>
       </FormControl>
--- frontend/src/schemas/listings.ts
+++ frontend/src/schemas/listings.ts
@@ -3,12 +3,49 @@ import type { RegisterOptions } from "react-hook-form"
 const RequiredFieldText = "Campo obrigatório"
 
 export interface ListingForm {
-  hourPricing: number
-  description: string
   title: string
+  type: string
+  quadro: string
+  aro: number
+  time: string
+  condition: string
+  brand: string
+  color: string
+  description: string
+  hourPricing: number
+
 }
 
 export const TitleValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const TypeValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const QuadroValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const AroValidation: RegisterOptions = {
+  required: RequiredFieldText,
+  valueAsNumber: true
+}
+
+export const TimeValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const ConditionValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const BrandValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const ColorValidation: RegisterOptions = {
   required: RequiredFieldText
 }
 
