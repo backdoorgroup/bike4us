b63b28caf5fbabb383b9a88feac66b4df49e73ba
rafaelres <rafaelsiqueirinha@gmail.com>
2023-10-25 00:48:24 -0300

    serializa todo mundo

--- backend/src/listings/serializers.ts
+++ backend/src/listings/serializers.ts
@@ -1,13 +1,5 @@
 import { Listing } from "@/listings/models"
 
 export const serializeListing = (listing: Listing) => ({
-  id: listing.id,
-  ownerUid: listing.ownerUid,
-  createdAt: listing.createdAt,
-  updatedAt: listing.updatedAt,
-  title: listing.title,
-  description: listing.description,
-  hourPricing: listing.hourPricing,
-  status: listing.status,
-  picturePath: listing.picturePath // TODO: isso aqui é um caminho absoluto na máquina do backend, tem que converter pra um caminho batendo no endpoint /static/nome-do-arquivo
+  ...listing
 })
