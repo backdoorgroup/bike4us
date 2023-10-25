ee3e33dfeaddc49ccb5a378b5f7d3d3c1bb3819f
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-10-25 18:06:39 -0300

    getters

--- frontend/config/settings.ts
+++ frontend/config/settings.ts
@@ -1,8 +1,20 @@
 export const settings = {
-  FB_API_KEY: import.meta.env._FB_API_KEY,
-  FB_AUTH_DOMAIN: import.meta.env._FB_AUTH_DOMAIN,
-  FB_PROJECT_ID: import.meta.env._FB_PROJECT_ID,
-  FB_STORAGE_BUCKET: import.meta.env._FB_STORAGE_BUCKET,
-  FB_MESSAGING_SENDER_ID: import.meta.env._FB_MESSAGING_SENDER_ID,
-  FB_APP_ID: import.meta.env._FB_APP_ID
+  get FB_API_KEY() {
+    return import.meta.env._FB_API_KEY
+  },
+  get FB_AUTH_DOMAIN() {
+    return import.meta.env._FB_AUTH_DOMAIN
+  },
+  get FB_PROJECT_ID() {
+    return import.meta.env._FB_PROJECT_ID
+  },
+  get FB_STORAGE_BUCKET() {
+    return import.meta.env._FB_STORAGE_BUCKET
+  },
+  get FB_MESSAGING_SENDER_ID() {
+    return import.meta.env._FB_MESSAGING_SENDER_ID
+  },
+  get FB_APP_ID() {
+    return import.meta.env._FB_APP_ID
+  }
 } as const
