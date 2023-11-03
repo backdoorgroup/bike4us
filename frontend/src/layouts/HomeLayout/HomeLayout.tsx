0e644cd5636730113e1fd6a16ea4513c1e5f0467
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-03 15:41:43 -0300

    separa layout

--- frontend/src/layouts/HomeLayout/HomeLayout.tsx
+++ frontend/src/layouts/HomeLayout/HomeLayout.tsx
@@ -1,21 +1,27 @@
-import Box from "@mui/material/Box"
+import "./HomeLayout.scss"
+
 import { Outlet, ScrollRestoration, useLocation } from "react-router-dom"
+
+import Box from "@mui/material/Box"
 
 import { AppNavBar, AppNavBottom } from "@/components"
 
-export function HomeLayout() {
+export default function HomeLayout() {
   const { pathname } = useLocation()
 
   const basic = pathname.includes("auth")
 
   return (
-    <>
+    <Box className="home-layout">
       <ScrollRestoration />
+
       <AppNavBar basic={basic} />
-      <Box component="main" sx={{ paddingBottom: 7 }}>
+
+      <Box className="hl-main" component="main">
         <Outlet />
       </Box>
+
       {!basic && <AppNavBottom />}
-    </>
+    </Box>
   )
 }
