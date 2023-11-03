d4a81c3a57b88abbbe75fca471afbdd94354cbc0
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-03 16:14:06 -0300

    error page

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,3 +1,5 @@
+import "./ErrorPage.scss"
+
 import { Link as RouterLink } from "react-router-dom"
 
 import Icon from "@mui/material/Icon"
@@ -5,12 +7,15 @@ import Link from "@mui/material/Link"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 
-export function ErrorPage() {
+export default function ErrorPage() {
   return (
-    <Container sx={{ paddingY: 4, textAlign: "center" }}>
-      <Icon sx={{ color: "primary.main", fontSize: "64px", display: "block", marginX: "auto", mb: 2 }}>error</Icon>
+    <Container className="error-page">
+      <Icon className="ep-icon">error</Icon>
+
       <Typography variant="h6">Esta página não está disponível</Typography>
+
       <Typography gutterBottom>Nos desculpe por isso.</Typography>
+
       <Link component={RouterLink} to="/">
         Ir para a página inicial
       </Link>
