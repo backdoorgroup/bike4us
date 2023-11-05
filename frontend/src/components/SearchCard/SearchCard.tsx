7abea8962d51f25761094f5d8c30da2db86373b5
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-04 23:01:01 -0300

    card

--- frontend/src/components/SearchCard/SearchCard.tsx
+++ frontend/src/components/SearchCard/SearchCard.tsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form"
 
 import Button from "@mui/material/Button"
 import Card from "@mui/material/Card"
-import CardContent from "@mui/material/CardContent"
 import FormControl from "@mui/material/FormControl"
 import Icon from "@mui/material/Icon"
 import TextField from "@mui/material/TextField"
@@ -32,36 +31,34 @@ export default function SearchCard({
 
   return (
     <Card className="search-card" variant="outlined">
-      <CardContent className="sc-content">
-        <FormControl
-          className="scc-form"
-          component="form"
+      <FormControl
+        className="sc-form"
+        component="form"
+        fullWidth
+        noValidate
+        autoComplete="off"
+        onSubmit={form.handleSubmit(handleSubmit)}>
+        <Stack className="scf-heading">
+          <Typography variant="h6">{title}</Typography>
+
+          <IconButton color="primary" size="small">
+            <Icon>tune</Icon>
+          </IconButton>
+        </Stack>
+
+        <TextField
+          className="scf-field"
+          label="Encontrar"
+          placeholder="O que você procura?"
+          helperText={form.formState.errors.query?.message}
           fullWidth
-          noValidate
-          autoComplete="off"
-          onSubmit={form.handleSubmit(handleSubmit)}>
-          <Stack className="sccf-heading">
-            <Typography variant="h6">{title}</Typography>
+          {...form.register("query", QueryValidation)}
+        />
 
-            <IconButton color="primary" size="small">
-              <Icon>tune</Icon>
-            </IconButton>
-          </Stack>
-
-          <TextField
-            className="sccf-field"
-            label="Encontrar"
-            placeholder="O que você procura?"
-            helperText={form.formState.errors.query?.message}
-            fullWidth
-            {...form.register("query", QueryValidation)}
-          />
-
-          <Button fullWidth disableElevation variant="contained" type="submit" endIcon={<Icon>search</Icon>}>
-            Encontrar
-          </Button>
-        </FormControl>
-      </CardContent>
+        <Button fullWidth disableElevation variant="contained" type="submit" endIcon={<Icon>search</Icon>}>
+          Encontrar
+        </Button>
+      </FormControl>
     </Card>
   )
 }
