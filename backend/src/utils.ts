0e2bbff8fd56e969be220c8fce03be63afc18879
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-05 23:37:28 -0300

    renaming and retyping paginate function

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,2 +1,2 @@
-export const paginate = <T>(list: T[], page: number, perPage: number) =>
-  list.slice(page * perPage - perPage, page * perPage)
+export const paginate = <T>(array: T[], page: number, perPage: number): T[] =>
+  array.slice(page * perPage - perPage, page * perPage)
