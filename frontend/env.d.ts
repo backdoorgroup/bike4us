f0500905ac2db7839b9307042ef7e438bba6986f
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-15 00:49:11 -0300

    readd env

---
+++ frontend/env.d.ts
@@ -0,0 +1,11 @@
+import type { PaletteOptions as MuiPaletteOptions, Palette as MuiPalette } from "@mui/material/styles"
+
+declare module "@mui/material/styles" {
+  interface PaletteOptions extends MuiPaletteOptions {
+    rating?: string
+  }
+
+  interface Palette extends MuiPalette {
+    rating?: string
+  }
+}
