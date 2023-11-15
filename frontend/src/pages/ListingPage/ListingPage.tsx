d856044570e685a99a193ffe56bb5bc7f33fd8b0
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-15 00:46:21 -0300

    começa a avaliar

--- frontend/src/pages/ListingPage/ListingPage.tsx
+++ frontend/src/pages/ListingPage/ListingPage.tsx
@@ -6,9 +6,13 @@ import { Suspense } from "react"
 import { Await, useLoaderData } from "react-router-dom"
 
 import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
 import Divider from "@mui/material/Divider"
+import Icon from "@mui/material/Icon"
+import LinearProgress from "@mui/material/LinearProgress"
 import Paper from "@mui/material/Paper"
+import Rating from "@mui/material/Rating"
 import Skeleton from "@mui/material/Skeleton"
 import Stack from "@mui/material/Stack"
 import Table from "@mui/material/Table"
@@ -21,18 +25,34 @@ import Typography from "@mui/material/Typography"
 import { ListingMap } from "~/components"
 import type { TListing, TLocations } from "~/schemas"
 import { BikeType, Condition, FrameSize, Material, WheelSize } from "~/schemas"
+import { useAuthStore } from "~/stores"
 
 export default function ListingPage() {
   const { listing, locations } = useLoaderData() as { listing: TListing; locations: Promise<TLocations> }
+  const { user } = useAuthStore()
 
   return (
     <Stack className="listing-page" divider={<Divider />}>
       <Container className="lp-section">
         <Stack className="lps-container lps-hero">
           <Box className="lpsh-header">
-            <Typography className="lpshh-condition" variant="caption">
-              {Condition[listing.condition]}
-            </Typography>
+            <Stack className="lpshh-headline">
+              <Typography className="lpshhh-condition" variant="caption">
+                {Condition[listing.condition]}
+              </Typography>
+
+              <Stack className="lpshhh-rating">
+                <Typography className="lpshhhr-text" variant="caption">
+                  4.5
+                </Typography>
+
+                <Rating readOnly defaultValue={4.5} precision={0.5} size="small" sx={{ color: "rating" }} />
+
+                <Typography className="lpshhhr-text" variant="caption">
+                  (48)
+                </Typography>
+              </Stack>
+            </Stack>
 
             <Typography>{listing.title}</Typography>
           </Box>
@@ -138,6 +158,139 @@ export default function ListingPage() {
               )}
             </Await>
           </Suspense>
+        </Stack>
+      </Container>
+
+      <Container className="lp-section">
+        <Stack className="lps-container">
+          <Typography variant="h6">Avaliação</Typography>
+
+          <Stack sx={{ gap: "16px", flexDirection: "row" }}>
+            {/* Começo do componente de avaliação */}
+            <Stack sx={{ gap: "8px", width: "100%" }}>
+              <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "8px" }}>
+                <Typography variant="caption" sx={{ color: "text.secondary", lineHeight: "1" }}>
+                  5
+                </Typography>
+
+                <LinearProgress
+                  variant="determinate"
+                  value={100}
+                  sx={{
+                    "width": "100%",
+                    "height": "8px",
+                    "borderRadius": "4px",
+                    "backgroundColor": "divider",
+                    "& span": { backgroundColor: "rating" }
+                  }}
+                />
+              </Stack>
+
+              <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "8px" }}>
+                <Typography variant="caption" sx={{ color: "text.secondary", lineHeight: "1" }}>
+                  4
+                </Typography>
+
+                <LinearProgress
+                  variant="determinate"
+                  value={78}
+                  sx={{
+                    "width": "100%",
+                    "height": "8px",
+                    "borderRadius": "4px",
+                    "backgroundColor": "divider",
+                    "& span": { backgroundColor: "rating" }
+                  }}
+                />
+              </Stack>
+
+              <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "8px" }}>
+                <Typography variant="caption" sx={{ color: "text.secondary", lineHeight: "1" }}>
+                  3
+                </Typography>
+
+                <LinearProgress
+                  variant="determinate"
+                  value={56}
+                  sx={{
+                    "width": "100%",
+                    "height": "8px",
+                    "borderRadius": "4px",
+                    "backgroundColor": "divider",
+                    "& span": { backgroundColor: "rating" }
+                  }}
+                />
+              </Stack>
+
+              <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "8px" }}>
+                <Typography variant="caption" sx={{ color: "text.secondary", lineHeight: "1" }}>
+                  2
+                </Typography>
+
+                <LinearProgress
+                  variant="determinate"
+                  value={42}
+                  sx={{
+                    "width": "100%",
+                    "height": "8px",
+                    "borderRadius": "4px",
+                    "backgroundColor": "divider",
+                    "& span": { backgroundColor: "rating" }
+                  }}
+                />
+              </Stack>
+
+              <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "8px" }}>
+                <Typography variant="caption" sx={{ color: "text.secondary", lineHeight: "1" }}>
+                  1
+                </Typography>
+
+                <LinearProgress
+                  variant="determinate"
+                  value={18}
+                  sx={{
+                    "width": "100%",
+                    "height": "8px",
+                    "borderRadius": "4px",
+                    "backgroundColor": "divider",
+                    "& span": { backgroundColor: "rating" }
+                  }}
+                />
+              </Stack>
+            </Stack>
+            {/* Fim do componente de avaliação */}
+
+            {/* Componente de média de avaliação */}
+            <Stack sx={{ alignItems: "center" }}>
+              <Typography variant="h3" sx={{ lineHeight: 1 }}>
+                4.5
+              </Typography>
+
+              <Rating readOnly defaultValue={4.5} precision={0.5} size="small" sx={{ color: "rating" }} />
+
+              <Typography variant="caption" sx={{ color: "text.secondary" }}>
+                48 avaliações
+              </Typography>
+            </Stack>
+          </Stack>
+
+          <Box>
+            <Button
+              disabled={!user}
+              startIcon={<Icon>rate_review</Icon>}
+              disableElevation
+              fullWidth
+              size="small"
+              variant="outlined">
+              Avalie
+            </Button>
+
+            {!user && (
+              <Typography variant="caption" sx={{ color: "action.disabled", lineHeight: "2" }}>
+                Você precisa estar logado para avaliar
+              </Typography>
+            )}
+          </Box>
         </Stack>
       </Container>
     </Stack>
