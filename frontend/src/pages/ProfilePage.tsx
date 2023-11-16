b7b4cb6e2f931bd6c927a794380a666e3241b6f2
rafaelres <rafaelsiqueirinha@gmail.com>
2023-11-16 19:58:18 -0300

    fix na importacao da store

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,8 +1,8 @@
-import { useAuthStore } from "@/stores"
+import { useAuthStore } from "~/stores"
 
 export function ProfilePage() {
   const { user } = useAuthStore()
-  console.log(user)
+
   return (
     <div>
       <img src={user?.photoURL || ""}></img>
