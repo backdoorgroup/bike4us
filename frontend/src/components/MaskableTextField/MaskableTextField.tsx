326d3632ebb172e57822b90ff2993a3043010063
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-21 21:45:36 -0300

    props

--- frontend/src/components/MaskableTextField/MaskableTextField.tsx
+++ frontend/src/components/MaskableTextField/MaskableTextField.tsx
@@ -1,9 +1,9 @@
+import type { ComponentProps } from "react"
 import { IMaskMixin } from "react-imask"
-import { ComponentProps } from "react"
 
 import TextField, { type TextFieldProps } from "@mui/material/TextField"
 
-export const InternalMaskableTextField = IMaskMixin((props) => <TextField {...props} />)
+export const InternalMaskableTextField = IMaskMixin((props) => <TextField {...(props as TextFieldProps)} />)
 
 export default function MaskableTextField(props: TextFieldProps & ComponentProps<typeof InternalMaskableTextField>) {
   return <InternalMaskableTextField {...props} />
