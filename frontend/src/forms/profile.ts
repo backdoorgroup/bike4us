94548c1453fcc5892c2f31c644be557905b81a8d
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-11-23 17:38:32 -0300

    funcional

--- frontend/src/forms/profile.ts
+++ frontend/src/forms/profile.ts
@@ -14,12 +14,27 @@ export interface AddressForm {
   zipcode: string
 }
 
+export interface PhoneNumberForm {
+  phoneNumber: string
+}
+
+export interface VerificationCodeForm {
+  verificationCode: string
+}
+
 export const ZipcodeValidation: RegisterOptions = {
   required: RequiredFieldText,
   validate: {
     length: (value) => value.length === 8 || "CEP deve ter 8 dígitos",
     valid: (value) => ValidZipcodePattern.test(value) || "CEP inválido"
   }
+}
+export const PhoneNumberValidation: RegisterOptions = {
+  required: RequiredFieldText
+}
+
+export const VerificationCodeValidation: RegisterOptions = {
+  required: RequiredFieldText
 }
 
 export const StreetValidation: RegisterOptions = {
--- frontend/src/pages/ProfilePhonePage/ProfilePhonePage.tsx
+++ frontend/src/pages/ProfilePhonePage/ProfilePhonePage.tsx
@@ -2,27 +2,37 @@ import "./ProfilePhonePage.scss"
 
 import type { User } from "firebase/auth"
 import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { Link as RouterLink } from "react-router-dom"
 
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
-import Step from "@mui/material/Step"
+import FormControl from "@mui/material/FormControl"
+import Link from "@mui/material/Link"
 import Stack from "@mui/material/Stack"
-import TextField from "@mui/material/TextField"
+import Step from "@mui/material/Step"
 import StepContent from "@mui/material/StepContent"
 import StepLabel from "@mui/material/StepLabel"
 import Stepper from "@mui/material/Stepper"
+import TextField from "@mui/material/TextField"
 import Typography from "@mui/material/Typography"
 
+import type { PhoneNumberForm, VerificationCodeForm } from "~/forms"
+import { PhoneNumberValidation, VerificationCodeValidation } from "~/forms"
 import { AuthServices } from "~/services"
 import { useAuthStore } from "~/stores"
 
 export default function ProfilePhonePage() {
-  const [activeStep, setActiveStep] = useState(0)
-  const [verificationId, setVerificationId] = useState("")
-  const [verificationCode, setVerificationCode] = useState("")
-  const [phoneNumber, setPhoneNumber] = useState("")
   const { user } = useAuthStore()
+
+  const [activeStep, setActiveStep] = useState(0)
+
+  // Isso deveria ter persistência
+  const [verificationId, setVerificationId] = useState("")
+
+  const phoneForm = useForm<PhoneNumberForm>()
+  const verificationForm = useForm<VerificationCodeForm>()
 
   const handleNext = () => {
     setActiveStep((prev) => prev + 1)
@@ -31,14 +41,14 @@ export default function ProfilePhonePage() {
     setActiveStep((prev) => prev - 1)
   }
 
-  const handleSubmitPhoneNumber = async () => {
+  const handleVerifyPhoneNumber = async ({ phoneNumber }: PhoneNumberForm) => {
     const verificationId = await AuthServices.verifyPhoneNumber(phoneNumber)
 
     setVerificationId(verificationId)
     handleNext()
   }
 
-  const handleUpdatePhoneNumber = async () => {
+  const handleUpdatePhoneNumber = async ({ verificationCode }: VerificationCodeForm) => {
     await AuthServices.updatePhoneNumber(user as User, verificationId, verificationCode)
 
     handleNext()
@@ -60,39 +70,73 @@ export default function ProfilePhonePage() {
         <Step>
           <StepLabel>Insira seu número de telefone</StepLabel>
 
-          <StepContent TransitionProps={{ unmountOnExit: false }} sx={{ mt: "8px" }}>
-            <TextField label="Número de celular" fullWidth sx={{ mb: "32px" }} />
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <FormControl
+              component="form"
+              fullWidth
+              noValidate
+              onSubmit={phoneForm.handleSubmit(handleVerifyPhoneNumber)}>
+              <TextField
+                label="Número de celular"
+                fullWidth
+                sx={{ mb: "32px" }}
+                {...phoneForm.register("phoneNumber", PhoneNumberValidation)}
+              />
 
-            <Stack sx={{ flexDirection: "row", gap: "16px", justifyContent: "flex-end" }}>
-              <Button disableElevation variant="contained" size="small" onClick={handleSubmitPhoneNumber}>
-                Próximo
-              </Button>
+              <Stack sx={{ flexDirection: "row", gap: "16px", justifyContent: "flex-end" }}>
+                <Button disableElevation variant="contained" size="small" type="submit">
+                  Próximo
+                </Button>
 
-              <Button disabled sx={{ mr: 1 }}>
-                Voltar
-              </Button>
-            </Stack>
+                <Button disabled sx={{ mr: 1 }}>
+                  Voltar
+                </Button>
+              </Stack>
+            </FormControl>
           </StepContent>
         </Step>
 
         <Step>
           <StepLabel>Verifique seu número de telefone</StepLabel>
 
-          <StepContent TransitionProps={{ unmountOnExit: false }} sx={{ mt: "8px" }}>
-            <TextField label="Código de confirmação" fullWidth sx={{ mb: "32px" }} />
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <FormControl
+              component="form"
+              fullWidth
+              noValidate
+              onSubmit={verificationForm.handleSubmit(handleUpdatePhoneNumber)}>
+              <TextField
+                label="Código de confirmação"
+                fullWidth
+                sx={{ mb: "32px" }}
+                {...verificationForm.register("verificationCode", VerificationCodeValidation)}
+              />
 
-            <Stack sx={{ flexDirection: "row", gap: "16px", justifyContent: "flex-end" }}>
-              <Button variant="contained" disableElevation size="small" onClick={handleUpdatePhoneNumber}>
-                Confirmar
-              </Button>
+              <Stack sx={{ flexDirection: "row", gap: "16px", justifyContent: "flex-end" }}>
+                <Button disableElevation variant="contained" size="small" type="submit">
+                  Confirmar
+                </Button>
 
-              <Button onClick={handleBack} sx={{ mr: 1 }}>
-                Voltar
-              </Button>
-            </Stack>
+                <Button onClick={handleBack} sx={{ mr: 1 }}>
+                  Voltar
+                </Button>
+              </Stack>
+            </FormControl>
           </StepContent>
         </Step>
       </Stepper>
+
+      {activeStep === 2 && (
+        <Box>
+          <Typography gutterBottom sx={{ mt: 2 }}>
+            Seu número de telefone foi verificado com sucesso.
+          </Typography>
+
+          <Link component={RouterLink} variant="subtitle1" to="/perfil">
+            Voltar à página de perfil
+          </Link>
+        </Box>
+      )}
     </Container>
   )
 }
